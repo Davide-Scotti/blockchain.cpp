036445af4c87ce21412384fc462af1d804317dfa
Davide Scotti <scotti.17115@gmail.com>
2024-05-14 08:16:36 +0000

    Creata ed implementata la classe CChain

--- CChain.h
+++ CChain.h
@@ -1,30 +1,21 @@
-#ifndef __C_BLOCK_INCLUDED__
-#define __C_BLOCK_INCLUDED__
-#include <string>
-#include <openssl/sha.h>
+#ifndef __C_CHAIN_INCLUDED__
+#define __C_CHAIN_INCLUDED__
+#include "CBlock.h"
+#include <vector>
 
 namespace blockchain
 {
-    class CBlock
+    class CChain
     {
         private:
-            uint8_t mHash[SHA256_DIGEST_LENGTH];
-            uint8_t mPrevHash[SHA256_DIGEST_LENGTH];
-            CBlock* mPrevBlock;
-            uint8_t mData;
-            uint32_t mDataSize;
-            time_t mCreatedTS;
-            uint32_t mNonce;
+            std::vector<CBlock> mChain;
+            CBlock* mCurrentBlock;
+            int mDifficulty;
         public:
-            CBlock(CBlock* mPrevBlock);
-            void calculateHash();
-            uint8_t* getHash();
-            std::string getHashStr();
-            CBlock* getPrevBlock();
-            void appendData(uint8_t data, uint32_t size);
-            bool isDifficulty(int difficulty);
-            void mine(int difficulty);
-            uint32_t getNonce();
+            CChain(int difficulty);
+            void appendToCurrentBlock(uint8_t data, uint32_t size);
+            void nextBlock();
+            CBlock* getCurrentBlock();
     };
 }
 
