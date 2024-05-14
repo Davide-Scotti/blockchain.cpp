b3a737190d5e10d0d83694402fc12a7d49ea2df6
Davide Scotti <scotti.17115@gmail.com>
2024-05-14 08:54:35 +0000

    sistemati alcuni bug

---
+++ build/CMakeFiles/simple-cpp-blockchain.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/blockchain.cpp/src/blockchain/CBlock.cpp" "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CBlock.cpp.o" "gcc" "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CBlock.cpp.o.d"
+  "/workspace/blockchain.cpp/src/blockchain/CChain.cpp" "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CChain.cpp.o" "gcc" "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CChain.cpp.o.d"
+  "/workspace/blockchain.cpp/src/main.cpp" "CMakeFiles/simple-cpp-blockchain.dir/src/main.cpp.o" "gcc" "CMakeFiles/simple-cpp-blockchain.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/simple-cpp-blockchain.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CBlock.cpp.o"
+  "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CBlock.cpp.o.d"
+  "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CChain.cpp.o"
+  "CMakeFiles/simple-cpp-blockchain.dir/src/blockchain/CChain.cpp.o.d"
+  "CMakeFiles/simple-cpp-blockchain.dir/src/main.cpp.o"
+  "CMakeFiles/simple-cpp-blockchain.dir/src/main.cpp.o.d"
+  "simple-cpp-blockchain"
+  "simple-cpp-blockchain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple-cpp-blockchain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
